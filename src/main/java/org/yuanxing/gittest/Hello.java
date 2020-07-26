3e09405277b7bb9f653ad7b643a69b1391e10b6f
yuanxing <18561703921@163.com>
2020-07-26 21:05:40 +0800

    第一次提交

---
+++ src/main/java/org/yuanxing/gittest/Hello.java
@@ -0,0 +1,14 @@
+package org.yuanxing.gittest;
+
+/**
+ * @author yuanxing
+ * @version 1.0.0
+ * @create 2020-07-26 20:47
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("HELLO GIT");
+    }
+}
