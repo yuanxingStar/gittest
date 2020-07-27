8721932daca333eefccfd9fd8978c70e117d2f61
yuanxing <18561703921@163.com>
2020-07-27 22:45:37 +0800

    [代码完善]((v 1.0.0)):
    第二次提交

---
+++ src/main/java/org/yuanxing/gittest/CommitMessagePlugins.java
@@ -0,0 +1,15 @@
+package org.yuanxing.gittest;
+
+/**
+ * @author yuanxing
+ * @version 1.0.0
+ * @create 2020-07-27 5:42
+ */
+public class CommitMessagePlugins {
+
+    public static void main(String[] args) {
+        System.out.println(
+                "测试IDEA插件 git提交时修改commit message 信息"
+        );
+    }
+}
